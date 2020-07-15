4505a6b3f2b197e939065180863760112f438bb2
juz <electronicallyreached@gmail.com>
2020-07-14 19:48:22 -0500

    Create LoginNav.js

---
+++ client/src/components/LoginNav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const LoginNav = () => {
+
+    return (
+        <nav className='level'>
+            <div className='level-item has-text-centered'>
+                <div>
+                    <a href='google.com'>Forgot Password?</a>
+                </div>
+            </div>
+            <div className='level-item has-text-centered'>
+                <div>
+                    <a href='google.com'>Create an Acount</a>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default LoginNav
