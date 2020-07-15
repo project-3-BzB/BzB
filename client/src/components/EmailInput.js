89fdcf1c203f38aaa95e49358e9611ef145ee70d
juz <electronicallyreached@gmail.com>
2020-07-14 19:47:05 -0500

    Create EmailInput.js

---
+++ client/src/components/EmailInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const EmailInput = () => {
+
+    return (
+        <input 
+        class="input is-medium is-rounded" 
+        type="email" 
+        placeholder="hello@example.com" 
+        autocomplete="username" 
+        required />
+    )
+}
+
+export default EmailInput
