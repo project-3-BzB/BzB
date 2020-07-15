e2f4548acd2a6d9bbcae0555c9e1e0e997b4c8b1
juz <electronicallyreached@gmail.com>
2020-07-14 22:10:59 -0500

    Update LoginBtn

--- client/src/components/LoginBtn.js
+++ client/src/components/LoginBtn.js
@@ -10,7 +10,8 @@ const LoginBtn = () => {
     //Return component jsx
     return (
         <button 
-        className='button is-block is-fullwidth is-primary is-medium is-rounded'
+        className='button is-block is-fullwidth is-primary is-medium is-rounded 
+        animate__animated animate__rubberBand animate__delay-2s'
         type='submit' onSubmit={handleSubmit}>
             Login
         </button>
