f83cf45c05a6040b23a09f65d49f2d922b20c3a0
juz <electronicallyreached@gmail.com>
2020-07-14 22:08:32 -0500

    Update Login.js

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -3,22 +3,27 @@ import React from 'react'
 import './style.css'
 import LoginForm from '../../components/LoginForm'
 import LoginNav from '../../components/LoginNav'
-import logo from '../../logo.png'
+import Logo from '../../components/Logo'
 
 
 //LOGIN PAGE MOCKUP
 
 const Login = () => {
+
     return (
-       <section className='hero is-fullheight'>
+        <div className='WrapperYellow'>
+            <section className='hero is-fullheight'>
            <div className='hero-body has-text-centered'>
-                <div className='login'>
-                    <img className='logo' src={logo} width='100px' alt='BzB logo' />
+                <div className='login animate__animated animate__backInDown'>
+                    <h4 className='title is-4'>Log In</h4>
+                    <Logo />
                     <LoginForm />
                     <LoginNav />
                 </div>
            </div>
        </section>
+        </div>
+       
     )
 }
 
