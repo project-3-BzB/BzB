21e58250745e49798033813d7fd88843c8752dfa
juz <electronicallyreached@gmail.com>
2020-07-14 19:47:22 -0500

    Create PWInput.js

---
+++ client/src/components/PWInput.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PWInput = () => {
+
+    return (
+        <input 
+        class="input is-medium is-rounded" 
+        type="password" 
+        placeholder="**********" 
+        autocomplete="current-password" 
+        required />
+    )
+}
+
+export default PWInput
