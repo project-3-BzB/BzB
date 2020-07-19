240c20d042db250bf430bef9910a72aceb49bab4
Dexter <kyleres@live.com>
2020-07-19 16:27:52 -0500

    backend fixes (user routes, user auth)

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const folderRoutes = require("./folderRoutes");
 
-// Book routes
 router.use("/user", userRoutes);
 router.use("/folder", folderRoutes);
 
--- server.js
+++ server.js
@@ -4,30 +4,31 @@ const session = require("express-session");
 const passport = require("passport");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const routes = require("./routes");
-const db = require("./models");
 const cors = require("cors");
+const routes = require("./routes");
 
 //renames express function to app
 const app = express();
 
-//set our port
+//set our port-------------------------------------------
 let PORT = process.env.PORT || 5000;
 
-//logger
+//logger for the terminal----------------------------------------------
 app.use(logger("dev"));
 
-//code for express
+//code for express---------------------------------------------------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static("public-sam"));
+
+//cors-----------------------------------------------------------------
 app.use(cors({
     origin: '*',
     optionsSuccessStatus: 200,
   }))
 
 //sync to mongoDB using mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bzb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bzbbackend", {
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
@@ -36,13 +37,18 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bzb", {
 //code for passport.js (user profile and login)--------------------------------
 app.use(session({ secret: "beeyoutiful", resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
-require("./config/passport");
 app.use(passport.session());
 
 //import routes here-----------------------------------
 app.use(routes);
 
-//start server
+//session info----------------------------------------------
+app.use( (req, res, next) => {
+    console.log('req.session', req.session);
+    return next();
+});
+
+//start server----------------------------------------
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
 });
@@ -51,16 +57,48 @@ app.listen(PORT, () => {
 const axios = require("axios");
 
 //test databse on mongo terminal - https://stackoverflow.com/questions/24985684/mongodb-show-all-contents-from-all-collections
-
+//---USER 1 Signup---
 // axios.post("http://localhost:5000/api/user/signup", {
-//     email: "blah@blah.com",
-//     username: "Blahblah",
-//     password: "blahblah"
+//     email: "abc@def.com",
+//     username: "again",
+//     password: "12345"
+// })
+// .then(function(res) {
+//     // console.log(res)
+// })
+// .catch(function(err) {
+//     // console.log(err)
 // })
 
+//---USER 1 Login---
 // axios.post("http://localhost:5000/api/user/login", {
-//     username: "Blahblah",
-//     password: "blahblah"
+//     username: "again",
+//     password: "12345"
+// })
+// .then(function(res) {
+//     // console.log(res)
+// })
+// .catch(function(err) {
+//     // console.log(err)
+// })
+
+//---USER 2 Signup---
+// axios.post("http://localhost:5000/api/user/signup", {
+//     email: "123@45sadas6.com",
+//     username: "",
+//     password: "12345"
+// })
+// .then(function(res) {
+//     // console.log(res)
+// })
+// .catch(function(err) {
+//     // console.log(err)
+// })
+
+//---USER 2 Login---
+// axios.post("http://localhost:5000/api/user/login", {
+//     username: "haha",
+//     password: "12345"
 // })
 // .then(function(res) {
 //     // console.log(res)
@@ -70,9 +108,9 @@ const axios = require("axios");
 // })
 
 // axios.post("http://localhost:5000/api/folder/create", {
-//     name: "Test 2"
+//     name: "Test"
 // }).then(function(res){
-//     console.log(res)
+//     // console.log(res)
 // }).catch(function(err){
-//     console.log(err)
+//     // console.log(err)
 // });
