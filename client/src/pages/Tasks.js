81ae39c7533bff5e180a75bc4b248bcf405dbb29
Marc Strong <marcstrong@yahoo.com>
2020-07-21 13:10:24 -0500

    clean up tasks page

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -4,7 +4,7 @@ import NavTiles from '../components/NavTiles';
 import FormHome from '../components/FormHome';
 import CardHome from '../components/CardHome';
 import {TileProvider} from '../utils/TileContext';
-import { BoxTask, Login } from '../style/Tasks'
+import { Login } from '../style/Tasks'
 
 import TaskList from '../components/TaskList';
 
@@ -25,93 +25,46 @@ function Tasks() {
         <FormHome />
 
         <div class="columns is-mobile is-centered">
-          <div class="column is-four-fifths">
-       
-          <BoxTask class="column">
-            <div class="notification is-link">
-              This container is centered on desktop.
-            </div>
-          </BoxTask>
-          <BoxTask class="column">
-            <div class="notification is-link">
-              This container is centered on desktop.
-            </div>
-          </BoxTask>
-          <BoxTask class="column">
-            <div class="notification is-link">
-              This container is centered on desktop.
-            </div>
-          </BoxTask>
-          <BoxTask class="column">
-            <div class="notification is-link">
-              This container is centered on desktop.
-            </div>
-          </BoxTask>
-          <BoxTask class="column">
-            <div class="notification is-link">
-              This container is centered on desktop.
-            </div>
-          </BoxTask>
-          <BoxTask class="column">
-            <div class="notification is-link">
-              This container is centered on desktop.
-            </div>
-          </BoxTask>
-        </div>
-        </div>
-
-
-      <div class="tile is-ancestor">
-        <div class="tile is-vertical is-8">
-          <div class="tile">
-            <div class="tile is-parent is-vertical">
-              <article class="tile is-child notification is-primary">
-                <p class="title">Vertical...</p>
-                <p class="subtitle">Top tile</p>
-              </article>
-              <article class="tile is-child notification is-warning">
-                <p class="title">...tiles</p>
-                <p class="subtitle">Bottom tile</p>
-              </article>
-            </div>
-            <div class="tile is-parent">
-              <article class="tile is-child notification is-info">
-                <p class="title">Middle tile</p>
-                <p class="subtitle">With an image</p>
-                <figure class="image is-4by3">
-                  <img src="https://bulma.io/images/placeholders/640x480.png"/>
-                </figure>
-              </article>
-            </div>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child notification is-danger">
-              <p class="title">Wide tile</p>
-              <p class="subtitle">Aligned with the right tile</p>
-              <div class="content">
+          <div class="column is-two-thirds">
           
+            <div class="column ">
+              
+              <div class="notification is-link ">
+                Left
               </div>
-            </article>
-          </div>
-        </div>
-        <div class="tile is-parent">
-          <article class="tile is-child notification is-success">
-            <div class="content">
-              <p class="title">Tall tile</p>
-              <p class="subtitle">With even more content</p>
-              <div class="content">
-      
+              </div>
+              <div class="column">
+              <div class="notification is-link ">
+                Right
               </div>
             </div>
-          </article>
+            <div class="column">
+              <div class="notification is-link">
+                This container is centered on desktop.
+              </div>
+            </div>
+            <div class="column">
+              <div class="notification is-link">
+                This container is centered on desktop.
+              </div>
+            </div>
+            <div class="column">
+              <div class="notification is-link">
+                This container is centered on desktop.
+              </div>
+            </div>
+            <div class="column">
+              <div class="notification is-link">
+                This container is centered on desktop.
+              </div>
+            </div>
+            <div class="column">
+              <div class="notification is-link">
+                This container is centered on desktop.
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <TileProvider>
-              {/* <div className="columns is-desktop is-mobile"> */}
-              <TaskList />
-              {/* </div> */}
-            </TileProvider>
 
 
           
