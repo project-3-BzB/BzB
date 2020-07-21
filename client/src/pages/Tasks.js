aad9c68fb0ec9d27cd2d322ca893032cc29d77e8
Marc Strong <marcstrong@yahoo.com>
2020-07-21 18:40:28 -0500

    fix folders display

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -2,11 +2,9 @@ import React from 'react'
 import './styles/Tasks.css'
 import NavTiles from '../components/NavTiles';
 import FormHome from '../components/FormHome';
-import CardHome from '../components/CardHome';
+import TaskFolder from '../components/TaskFolder';
 import {TileProvider} from '../utils/TileContext';
 import { Login } from '../style/Tasks'
-
-import TaskList from '../components/TaskList';
 
 function Tasks() {
 
@@ -14,8 +12,8 @@ function Tasks() {
     <div>
       <div className="wrapperBlue">
 
-        <div class="tile is-ancestor">
-          <div class="tile is-parent is-vertical is-12">
+        <div className="tile is-ancestor">
+          <div className="tile is-parent is-vertical is-12">
             <TileProvider>
               <NavTiles />
             </TileProvider>
@@ -24,52 +22,20 @@ function Tasks() {
 
         <FormHome />
 
-        <div class="columns is-mobile is-centered">
-          <div class="column is-two-thirds">
-          
-            <div class="column ">
-              
-              <div class="notification is-link ">
-                Left
-              </div>
-              </div>
-              <div class="column">
-              <div class="notification is-link ">
-                Right
-              </div>
-            </div>
-            <div class="column">
-              <div class="notification is-link">
-                This container is centered on desktop.
-              </div>
-            </div>
-            <div class="column">
-              <div class="notification is-link">
-                This container is centered on desktop.
-              </div>
-            </div>
-            <div class="column">
-              <div class="notification is-link">
-                This container is centered on desktop.
-              </div>
-            </div>
-            <div class="column">
-              <div class="notification is-link">
-                This container is centered on desktop.
-              </div>
-            </div>
-            <div class="column">
-              <div class="notification is-link">
-                This container is centered on desktop.
-              </div>
-            </div>
+
+        <div className="columns is-ancestor is-mobile is-centered">
+          <div className="column is-two-thirds ">
+            <TaskFolder />
+            <TaskFolder />
+            <TaskFolder />
+            <TaskFolder />
+            <TaskFolder />
+            <TaskFolder />
           </div>
         </div>
 
-
-          
-            </div>
       </div>
+    </div>
   );
 }
 
